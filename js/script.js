5603c6a36801c3ef37d2142b0b14947a398fca6c
Roberto Peixinho <rhpeixinho@gmail.com>
2022-05-23 19:49:37 -0300

    Botões funcionais

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const infoBtn = document.getElementById('info');
+const breakingbad = document.getElementById('brakingbad');
+
+infoBtn.addEventListener ('click', () => breakingbad.classList.toggle("showPage"));
+
+
+
+
+
+
+
+
+
